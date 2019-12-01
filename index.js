f80f5e2fe2ebe5754b5f00fed52ea0801487ab47
NIKHIL KUMAR GAUTAM <44256345+nikhilkrgautam@users.noreply.github.com>
2019-12-01 13:06:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+
 io.on('connection', function(socket){
     console.log('a user connected');
     socket.on('disconnect', function(){
       console.log('user disconnected');
     });
   });
+
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
       io.emit('chat message', msg);

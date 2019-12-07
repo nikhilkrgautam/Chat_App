e80a426a3d7fca3489d0d8801a7a72ea796d7496
NIKHIL KUMAR GAUTAM <44256345+nikhilkrgautam@users.noreply.github.com>
2019-12-07 21:27:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
       io.emit('chat message', msg);
